3f65ddb8aea4e5a57af144014e7428e36f1bb1d5
Vasiliy <kkolyasev1228@gmail.com>
2022-02-25 11:45:16 +0400

    start tests

--- config/db.php
+++ config/db.php
@@ -3,12 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
+    'username' => '',
     'password' => '',
     'charset' => 'utf8',
-
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
 ];

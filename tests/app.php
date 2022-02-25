045fd386665ef65766d82e6a8d6c53f73f9c6786
Vasiliy <kkolyasev1228@gmail.com>
2022-02-25 15:18:48 +0400

    unit

--- tests/app.php
+++ tests/app.php
@@ -2,68 +2,21 @@
 
 namespace tests;
 
-use app\models\User;
-
 require(__DIR__ . '/_bootstrap.php');
 
-class UserTest extends TestCase
-{
-    public function setUp()
-    {
-        parent::setUp();
-        User::deleteAll([
-            'username' => 'username',
-            'email' => 'email@email.ru',
-        ]);
-    }
-
-    public function testValidateEmptyValues()
-    {
-        $user = new User();
-        $this->assertFalse($user->validate(), 'model is not valid');
-        $this->assertArrayHasKey('username', $user->getErrors(), 'check username error');
-        $this->assertArrayHasKey('email', $user->getErrors(), 'check email error');
-    }
-
-    public function testValidateWrongValues()
-    {
-        $user = new User([
-            'username' => 'username :(',
-            'email' => 'email',
-        ]);
-        $this->assertFalse($user->validate(), 'validate incorrect username and email');
-        $this->assertArrayHasKey('username', $user->getErrors(), 'check incorrect username error');
-        $this->assertArrayHasKey('email', $user->getErrors(), 'check incorrect email error');
-    }
-
-    public function testValidateCorrectValues()
-    {
-        $user = new User([
-            'username' => 'username',
-            'email' => 'email@email.ru',
-        ]);
-        $this->assertTrue($user->validate(), 'correct model is valid');
-    }
-
-    public function testSaveIntoDatabase()
-    {
-        $user = new User([
-            'username' => 'username',
-            'email' => 'email@email.ru'
-        ]);
-
-        $this->assertTrue($user->save(), 'model is saved');
-    }
-}
-
-$class = new \ReflectionClass('\tests\UserTest');
-foreach ($class->getMethods() as $method) {
-    if (substr($method->name, 0, 4) == 'test') {
-        echo 'Test ' . $method->class . '::' . $method->name . PHP_EOL . PHP_EOL;
-        $test = new $method->class;
-        $test->setUp();
-        $test->{$method->name}();
-        $test->tearDown();
-        echo PHP_EOL;
+foreach (scandir(__DIR__ . '/unit') as $file) {
+    if (substr($file, -8, 8) == 'Test.php') {
+        $className = pathinfo($file, PATHINFO_FILENAME);
+        $class = new \ReflectionClass('\tests\unit\\' . $className);
+        foreach ($class->getMethods() as $method) {
+            if (substr($method->name, 0, 4) == 'test') {
+                echo 'Test ' . $method->class . '::' . $method->name . PHP_EOL . PHP_EOL;
+                $test = new $method->class;
+                $test->setUp();
+                $test->{$method->name}();
+                $test->tearDown();
+                echo PHP_EOL;
+            }
+        }
     }
 }

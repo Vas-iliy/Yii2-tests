a9446fd6228f0eaa124bc066fbbd92c687792f90
Vasiliy <kkolyasev1228@gmail.com>
2022-02-25 12:27:33 +0400

    end test

--- tests/app.php
+++ tests/app.php
@@ -1,5 +1,33 @@
 <?php
 
+namespace tests;
+
+use app\models\User;
+
 require(__DIR__ . '/_bootstrap.php');
 
-echo Yii::$app->name . PHP_EOL;
+class UserTest
+{
+    protected function assert($condition, $message = '')
+    {
+        echo $message;
+        if ($condition) {
+            echo  ' Ok' . PHP_EOL;
+        } else {
+            echo ' Fail' . PHP_EOL;
+            exit();
+        }
+    }
+
+    public function testValidateEmptyValues()
+    {
+        $user = new User();
+
+        $this->assert($user->validate() == false, 'model is not valid');
+        $this->assert(array_key_exists('username', $user->getErrors()), 'check username error');
+        $this->assert(array_key_exists('email', $user->getErrors()), 'check email error');
+    }
+}
+
+$test = new UserTest();
+$test->testValidateEmptyValues();
